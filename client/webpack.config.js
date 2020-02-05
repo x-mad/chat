a36320f40d2c0613955494fcfc281f514ef36482
x-mad <eugene.kochura@gmail.com>
2020-02-05 02:09:05 +0200

    webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry : {
+        app: './src/index.js'
+    },
+    output : {
+        filename: '[name].js',
+        path : path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules : [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node-modules/'
+        }]
+    },
+
+    devServer: {
+        overlay: true
+    }
+}
